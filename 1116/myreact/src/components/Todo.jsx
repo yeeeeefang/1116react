5f702be8e4209369f7d1c9a4709fe2d510ccb6e0
yeeeeefang <sweetcat881314@gmail.com>
2024-11-19 10:21:00 +0800

    1119

--- 1116/myreact/src/components/Todo.jsx
+++ 1116/myreact/src/components/Todo.jsx
@@ -1,10 +1,20 @@
+import { MdDelete, MdModeEdit } from "react-icons/md";
+
 //解構props的屬性名稱，用{屬姓名}包起來
-function Todo({todo}) {
+function Todo({ todo, deleteTodo }) {
     return (
         <div className="todo">
             {/* <p>繳停車費</p> */}
             {/* <p>{todo}</p> */}
             <p>{todo.content}</p>
+            <div>
+                <MdModeEdit style={{ cursor: 'pointer' }} />
+                <MdDelete
+                    onClick={() => { deleteTodo(todo.id) }}
+                    style={{ cursor: 'pointer', marginLeft: '5px' }} />
+            </div>
+
+
         </div>
     )
 }
--- 1116/myreact/src/components/TodoWrappe.jsx
+++ 1116/myreact/src/components/TodoWrappe.jsx
@@ -17,16 +17,29 @@ function TodoWrapper() {
     //1.使用...其餘運算子來保留原陣列內容
     //2.再加入新的物件內容
     //第一個content是屬性名稱 第二個content是屬性值
-    const addTodo = (content)=>{
-        setTodos([...todos,{content: content, id: Math.random() }])
+    const addTodo = (content) => {
+        setTodos([...todos, { content: content, id: Math.random() }])
     }
+
+    //建立刪除todo函式，要傳給Todo元件使用
+    //使用filter方法，去除被刪除的todo
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => {
+            //檢查目前的id是否為被刪除的id
+            //如果不是則保留
+            return todo.id !== id;
+        }))
+    }
+
     return (
         <div className="wrapper">
             <h1>待辦事項</h1>
-            <CreateForm addTodo={addTodo}/>
+            <CreateForm addTodo={addTodo} />
             {
                 todos.map((todo) => {
-                    return <Todo todo={todo} key={todo.id} />
+                    return <Todo todo={todo} key={todo.id}
+                        deleteTodo={deleteTodo}
+                    />
                 })
             }
 
