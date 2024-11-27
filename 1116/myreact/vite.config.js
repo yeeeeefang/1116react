e7bcf8e84adcaf5fea7b7fb9a3eac84bcbf1273e
yeeeeefang <sweetcat881314@gmail.com>
2024-11-27 13:12:20 +0800

    1127

--- 1116/myreact/vite.config.js
+++ 1116/myreact/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   //建立一個開發中的路徑
   //process.env.NODE_ENV =>取得node環境中的變數
   //如果目前的路徑等於產品路徑，就執行倉庫的路徑
-  base: process.env.NODE_ENV === 'production' ? '/1116react.1116.myreact/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/1116react>1116>myreact/' : '/',
   plugins: [react()],
 })
